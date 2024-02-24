b248d74ac9f10540756a7fc564f16fdc810a947c
Ellie Roberts <ellieautumn1@outlook.com>
2024-02-24 16:30:21 -0700

    generates random code

--- mastermind_html/board.js
+++ mastermind_html/board.js
@@ -1,1 +1,20 @@
-lorem ipsum  
+function generateSecretCode() {
+    const colors = ['red', 'green', 'yellow', 'blue', 'black', 'white'];
+    let secretCode = [];
+    for (let i = 0; i < 4; i++) {
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        secretCode.push(colors[randomIndex])
+    }
+}
+
+let secretCode = generateSecretCode();
+let attempts = 0;
+
+let selectedColor;
+let selectedSlot;
+
+function selectColor(color){
+    selectedColor = color;
+
+    if (selectedSlot)
+}
