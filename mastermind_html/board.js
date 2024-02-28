42fb26c8674f868ffca1e385d03736223589ef37
Ellie Roberts <ellieautumn1@outlook.com>
2024-02-28 01:48:57 -0700

    stats page

--- mastermind_html/board.js
+++ mastermind_html/board.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    let R = "linear-gradient(to top right, red, rgb(168, 121, 121))"
+    let W = "linear-gradient(to top right, rgb(97, 96, 96), white)"
 
     let slotDefault = "rgba(0, 0, 0, 0) linear-gradient(to right top, rgb(53, 50, 50), rgb(128, 128, 128)) repeat scroll 0% 0% / auto padding-box border-box";
     let selectedColor = '';
@@ -163,6 +165,9 @@ $(document).ready(function() {
         if (reds === 0 && whites === 1) {
             $(`.${guess + 1}-1`).css("background", W);
         }
+        if (reds === 1 && whites === 0) {
+            $(`.${guess + 1}-1`).css("background", R);
+        }
         if (reds === 0 && whites === 2) {
             $(`.${guess + 1}-1`).css("background", W);
             $(`.${guess + 1}-2`).css("background", W);
@@ -266,13 +271,36 @@ $(document).ready(function() {
 
         }
         
+
+        function saveScore(guess) {
+            // Retrieve existing scores from local storage
+            let existingScores = localStorage.getItem('gameScores');
+        
+            // If there are no existing scores, create an empty array
+            if (!existingScores) {
+                existingScores = [];
+            } else {
+                // Parse the existing scores from JSON
+                existingScores = JSON.parse(existingScores);
+            }
+             guess += 1;
+            // Add the new score to the array
+            existingScores.push(guess);
+        
+            // Save the updated scores back to local storage
+            localStorage.setItem('gameScores', JSON.stringify(existingScores));
+        }
+
         if (guess <= 9) {
             openModal1();
+            saveScore(guess);
         }
         else {
             openModal2();
+            saveScore(guess);
         }
     };
 
+    
 
 });
--- mastermind_html/stats.js
+++ mastermind_html/stats.js
@@ -1,1 +1,82 @@
-lorem ipsum
+let existingScores = localStorage.getItem('gameScores');
+if (!existingScores) {
+    existingScores = [];
+} else {
+    // Parse the existing scores from JSON
+    existingScores = JSON.parse(existingScores);
+}
+console.log(existingScores)
+
+let ones = 0;
+let twos = 0;
+let threes = 0;
+let fours = 0;
+let fives = 0;
+let sixes = 0;
+let sevens = 0;
+let eights = 0;
+let nines = 0;
+let tens = 0;
+
+let total = 0;
+
+let maxWidth = 1000;
+
+for (score of existingScores) {
+    total += score;
+
+    if (score == 1) {
+        ones += 1;
+    }
+    if (score == 2) {
+        twos += 1;
+    }
+    if (score == 3) {
+        threes += 1;
+    }
+    if (score == 4) {
+        fours += 1;
+    }
+    if (score == 5) {
+        fives += 1;   
+    }
+    if (score == 6) {
+        sixes += 1;   
+    }
+    if (score == 7) {
+        sevens += 1;  
+    }
+    if (score == 8) {
+        eights += 1;  
+    }
+    if (score == 9) {
+        nines += 1;
+    }
+    if (score == 10) {
+        tens += 1;
+    }
+}
+let onesPc = ones / total;
+$(".ones").css("width", maxWidth * onesPc + 'px');
+let twosPc = twos / total;
+$(".twos").css("width", maxWidth * twosPc + 'px');
+let threesPc = threes / total;
+$(".threes").css("width", maxWidth * threesPc + 'px');
+let foursPc = fours / total;
+$(".fours").css("width", maxWidth * foursPc + 'px');
+let fivesPc = fives/ total;
+$(".fives").css("width", maxWidth * fivesPc + 'px');
+let sixesPc = sixes / total;
+$(".sixes").css("width", maxWidth * sixesPc + 'px');
+let sevensPc = sevens / total;
+$(".sevens").css("width", maxWidth * sevensPc + 'px');
+let eightsPc = eights / total;
+$(".eights").css("width", maxWidth * eightsPc + 'px');
+let ninesPc = nines / total;
+$(".nines").css("width", maxWidth * ninesPc + 'px');
+let tensPc = tens / total;
+$(".tens").css("width", maxWidth * tensPc + 'px');
+
+
+
+
