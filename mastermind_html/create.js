e5f51cab8e8a54fee5fa7eaa226c73d7e60464e6
Ellie Roberts <ellieautumn1@outlook.com>
2024-02-28 23:22:22 -0700

    save username

--- mastermind_html/create.js
+++ mastermind_html/create.js
@@ -1,1 +1,6 @@
-rgrg
+function login() {
+    const user = document.querySelector("#username");
+    localStorage.setItem("userName", user.value);
+    window.location.href = "instructions.html";
+  }
+  
